56abd6fca6988298e3df14ddf497135f49aad294
Ran <ran.hassid@gmail.com>
2024-05-16 13:30:38 +0300

    create and delete address

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -5,11 +5,21 @@ sap.ui.define(
     "sap/m/Page",
     "sap/m/Button",
     "sap/f/LayoutType",
+    "ui5/demo/ui5demo/model/models",
+    "sap/m/MessageToast",
   ],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller, JSONModel, Page, Button, LayoutType) {
+  function (
+    Controller,
+    JSONModel,
+    Page,
+    Button,
+    LayoutType,
+    models,
+    MessageToast
+  ) {
     "use strict";
 
     return Controller.extend("ui5.demo.ui5demo.controller.Detail", {
@@ -27,10 +37,91 @@ sap.ui.define(
 
       onRouteMatched: function (oEvent) {
         const oArgs = oEvent.getParameter("arguments");
-        this.getView().byId("currentId").setText(oArgs.id);
+        this._userId = oArgs.id;
+
+        const oView = this.getView();
+        oView.bindElement(`/UserSet('${this._userId}')`);
 
         const oLayoutModel = this.getOwnerComponent().getModel("layoutModel");
         oLayoutModel.setProperty("/layout", LayoutType.TwoColumnsMidExpanded);
+      },
+
+      onCreateUserAddress: async function (oEvent) {
+        this._oDialog = await this.loadFragment({
+          name: "ui5.demo.ui5demo.fragment.CreateUserAddress",
+        });
+
+        const oUserAddressModel = new JSONModel({
+          busy: false,
+          userAddress: {
+            addressType: "",
+            address: "",
+            city: "",
+            country: "",
+          },
+        });
+
+        this._oDialog.setModel(oUserAddressModel);
+        this._oDialog.setModel(models.createAddressTypeModel(), "addressTypes");
+
+        this._oDialog.open();
+
+        // Open the dialog in an alternative way
+
+        // this.loadFragment({
+        //   name: "ui5.demo.ui5demo.fragment.CreateUserAddress",
+        // }).then(function (oDialog) {
+        //   oDialog.open();
+        // });
+      },
+
+      onSaveUserAddress: function (oEvent) {
+        const oData = this._oDialog.getModel().getProperty("/userAddress");
+
+        this._oDialog.getModel().setProperty("/busy", true);
+
+        const oDataModel = this.getOwnerComponent().getModel();
+        oDataModel.create(`/UserSet('${this._userId}')/addresses`, oData, {
+          success: (oData, response) => {
+            this._oDialog.getModel().setProperty("/busy", false);
+            MessageToast.show("Addreess added successfully!");
+            this._oDialog.close();
+          },
+          error: (oError) => {
+            this._oDialog.getModel().setProperty("/busy", false);
+            alert(oError.message);
+          },
+        });
+      },
+      onDeleteUserAddress: function (oEvent) {
+        const oObjectToDelete = oEvent
+          .getSource()
+          .getBindingContext()
+          .getObject();
+        const oDataModel = this.getOwnerComponent().getModel();
+        const sPath = oDataModel.createKey("UserAddressSet", {
+          id: oObjectToDelete.id,
+          addressType: oObjectToDelete.addressType,
+        });
+
+        oDataModel.remove(`/${sPath}`, {
+          success: (oData, response) => {
+            MessageToast.show("Addreess deleted successfully!");
+          },
+          error: (oError) => {
+            alert(oError.message);
+          },
+        });
+      },
+
+      onDialogClose: function (oEvent) {
+        this._oDialog.destroy();
+        this._oDialog = null;
+      },
+
+      onCloseUserAddressDialog: function (oEvent) {
+        // oEvent.getSource().getParent().close();
+        this._oDialog.close();
       },
 
       onRouteMainMatched: function (oEvent) {},
--- webapp/model/models.js
+++ webapp/model/models.js
@@ -1,23 +1,44 @@
-sap.ui.define([
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/Device"
-], 
-    /**
-     * provide app-view type models (as in the first "V" in MVVC)
-     * 
-     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
-     * @param {typeof sap.ui.Device} Device
-     * 
-     * @returns {Function} createDeviceModel() for providing runtime info for the device the UI5 app is running on
-     */
-    function (JSONModel, Device) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/model/json/JSONModel", "sap/ui/Device"],
+  /**
+   * provide app-view type models (as in the first "V" in MVVC)
+   *
+   * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+   * @param {typeof sap.ui.Device} Device
+   *
+   * @returns {Function} createDeviceModel() for providing runtime info for the device the UI5 app is running on
+   */
+  function (JSONModel, Device) {
+    "use strict";
 
-        return {
-            createDeviceModel: function () {
-                var oModel = new JSONModel(Device);
-                oModel.setDefaultBindingMode("OneWay");
-                return oModel;
-        }
+    return {
+      createDeviceModel: function () {
+        var oModel = new JSONModel(Device);
+        oModel.setDefaultBindingMode("OneWay");
+        return oModel;
+      },
+      createAddressTypeModel: function () {
+        const oModel = new JSONModel([
+          {
+            key: "home",
+            text: "Home",
+          },
+          {
+            key: "office",
+            text: "Office",
+          },
+          {
+            key: "work",
+            text: "Work",
+          },
+          {
+            key: "abroad",
+            text: "Abroad",
+          },
+        ]);
+
+        return oModel;
+      },
     };
-});
+  }
+);
